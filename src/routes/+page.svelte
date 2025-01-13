d64a8900b19b34ae66031155173bd65eca5e71c0
tiberiu.iorgulescu@hootsuite.com <tiberiu.iorgulescu@hootsuite.com>
2025-01-14 00:19:42 +0200

    use global css property

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,8 @@
 <script>
     import SequentialTraining from '/src/components/SequentialTraining.svelte';
-	import DarkThemeButton from '/src/components/DarkThemeButton.svelte';
+    import DarkThemeButton from '/src/components/DarkThemeButton.svelte';
     import {chapters} from '/src/data/verses';
+	import { onMount } from 'svelte';
 
     let bookName = 'Filipeni'
     let start = {
@@ -10,72 +11,70 @@
     }
     let bookEntries = Object.entries(chapters)
 
-	let currentDate = new Date();
-    let currentHour = currentDate.getHours();
-	let theme = 'light-mode';
-	if (currentHour >= 22 || currentHour <= 7) {
-        theme = 'dark-mode';
-    }
+	onMount(() => {
+		let currentDate = new Date();
+		let currentHour = currentDate.getHours();
+		if (currentHour >= 22 || currentHour <= 7) {
+			window.document.body.classList.add('dark-mode')
+		}
+	});
 </script>
 
-<body class:dark-mode={theme==='dark-mode'}>
-	<h1>Romanii</h1>
-	<label for="books">Cartea:</label>
-	<select bind:value={bookName}>
-		{#each bookEntries as [name, chapter]}
-			<option value={name}>{name}</option>
-		{/each}
-	</select>
-	<SequentialTraining bookName={bookName} chapters={chapters[bookName]} start={start}/>
-	<br><br>
-	<a href="/randomverses">Versete aleatoare</a>
-	<br>
-	<DarkThemeButton />
-</body>
-
+<h1>Romanii</h1>
+<label for="books">Cartea:</label>
+<select bind:value={bookName}>
+	{#each bookEntries as [name, chapter]}
+		<option value={name}>{name}</option>
+	{/each}
+</select>
+<SequentialTraining bookName={bookName} chapters={chapters[bookName]} start={start}/>
+<br><br>
+<a href="/randomverses">Versete aleatoare</a>
+<br>
+<DarkThemeButton />
 
 <!-- TODO: find a way to have dark-mode styles in a single file -->
 <style>
-	:global(body) {
-		transition: background-color 0.3s;
-	}
+    :global(body) {
+        transition: background-color 0.3s;
+    }
 
-	:global(body.dark-mode) {
-		background-color: #10171d;
-		color: #d7d7d7;
-	}
+    :global(body.dark-mode) {
+        background-color: #10171d;
+        color: #d7d7d7;
+    }
 
-	/* Dark mode styles for inputs (textboxes) */
-	:global(body.dark-mode input, body.dark-mode textarea, body.dark-mode select) {
-		background-color: #192028;
-		/* Darker background for inputs */
-		color: #d7d7d7;
-		/* Lighter text color */
-		border: 1px solid #3a4c61;
-		/* Subtle border for contrast */
-	}
+    /* Dark mode styles for inputs (textboxes) */
+    :global(body.dark-mode input, body.dark-mode textarea, body.dark-mode select) {
+        background-color: #192028;
+        /* Darker background for inputs */
+        color: #d7d7d7;
+        /* Lighter text color */
+        border: 1px solid #3a4c61;
+        /* Subtle border for contrast */
+    }
 
-	/* Dark mode styles for buttons */
-	:global(body.dark-mode button) {
-		background-color: #1a2e45;
-		/* Darker background for buttons */
-		color: #d7d7d7;
-		/* Lighter text color */
-		border: 1px solid #3a4c61;
-		/* Subtle border for contrast */
-	}
+    /* Dark mode styles for buttons */
+    :global(body.dark-mode button) {
+        background-color: #1a2e45;
+        /* Darker background for buttons */
+        color: #d7d7d7;
+        /* Lighter text color */
+        border: 1px solid #3a4c61;
+        /* Subtle border for contrast */
+    }
 
-	/* Button hover effect for a slightly lighter color */
-	:global(body.dark-mode button:hover) {
-		background-color: #3a4c61;
-		/* Lighter background color on hover */
-	}
+    /* Button hover effect for a slightly lighter color */
+    :global(body.dark-mode button:hover) {
+        background-color: #3a4c61;
+        /* Lighter background color on hover */
+    }
 
-	/* For input focus */
-	:global(body.dark-mode input:focus, body.dark-mode textarea:focus) {
-		border-color: #4a5b70;
-		/* Slightly lighter border on focus */
-		outline: none;
-		/* Remove the default outline */
-	}
+    /* For input focus */
+    :global(body.dark-mode input:focus, body.dark-mode textarea:focus) {
+        border-color: #4a5b70;
+        /* Slightly lighter border on focus */
+        outline: none;
+        /* Remove the default outline */
+    }
 </style>
